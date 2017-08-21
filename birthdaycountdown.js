d11fdbafc0ff83339251592327ae70064778ba74
Tiffani Fox <teethefox@gmail.com>
2017-08-21 15:10:36 -0700

    Birthday Countdown assignment added

---
+++ birthdaycountdown.js
@@ -0,0 +1,14 @@
+function daysUntilMyBirthday(days) {
+  for (var i = days; i >= 0; i--){
+ if (i < 30)
+console.log(i + "DAYS UNTIL MY BIRTHDAY! ALMOST HERE!!");
+ if(i===0)
+  console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«" );
+  else {
+    console.log(i +"days until my birthday. C'mon already!!!! D':");
+
+    }
+  }
+}
+
+console.log(daysUntilMyBirthday());
